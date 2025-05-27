652424c2d5546056090ec0417be7900b1fb980eb
Jorge <jupiter.adicto@gmail.com>
2025-05-27 21:28:12 +0200

    probandoContador4

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Jupiter Projects</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
+        <button onClick={() => setCount((count) => count + 3)}>
           count is {count}
         </button>
         <p>
